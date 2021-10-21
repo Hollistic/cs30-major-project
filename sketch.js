c7b2fc33c863c9d79be82886d5144b830f7602d9
Umair Khan <umairk2019@gmail.com>
2021-10-21 11:01:19 -0600

    ship movement physics working
    need to work on virtual camera

--- sketch.js
+++ sketch.js
@@ -3,7 +3,8 @@
 // 10/19/2021
 //
 // Extra for Experts:
-// - used p5.play library
+// - used a new library called p5.play
+// - used a virtual camera to create an "open world" experience
 
 let sceneW;
 let sceneH;
@@ -11,8 +12,6 @@ let sceneH;
 //ship values
 let ship;
 let shipImage;
-let shipWidth = 50;
-let shipHeight = 50;
 
 function setup() {
   //create window
@@ -25,22 +24,71 @@ function setup() {
 
   sceneW = width + width/2;
   sceneH = height + height/2;
-  
+
+
   //load ship
-  shipImage = loadImage("assets/ship1.png");
+  shipRestImg = loadImage("assets/ship1.png");
+  shipThrustImg = loadImage("assets/ship1-thrust.png");
   createShip(width/2, height/2, 50, 50, 5);
+
+  //create asteroids group
+  asteroids = new Group();
+
+  //create background group
+  rocksImg = loadImage("assets/rock.png");
+  bg = new Group();
+
+  createBG();
 }
 
 function draw() {
-  background(220);
+  clear();
+  background(0);
+
+  shipControls();
   drawSprites();
+  // controlCamera();
 }
 
-function createShip(x, y, width, height, speed) {
-  ship = createSprite(x, y, width, height);
-  ship.addImage("static", shipImage);
-  ship.addAnimation("accelerating", "assets/ship1.thrust");
-  ship.setCollider("rectangle", 0, 0, shipWidth, shipHeight);
-  ship.maxSpeed(speed);
-  ship.friction(0.99);
+//creates a new ship
+function createShip(x, y, w, h, mSpeed) {
+  ship = createSprite(x, y, w, h);
+  ship.addImage("resting", shipRestImg);
+  ship.addAnimation("accelerating", shipThrustImg);
+  ship.setCollider("rectangle", 0, 0, w, h);
+  ship.maxSpeed = mSpeed;
+  ship.friction = 0.02;
+}
+
+function shipControls() {
+  if (keyDown("W")) {
+    ship.changeAnimation("accelerating");
+    ship.addSpeed(0.1, ship.rotation);
+  }
+  else {
+    ship.changeAnimation("resting");
+  }
+  
+  if (keyDown("A")) {
+    ship.rotation -= 3;
+  }
+
+  if (keyDown("D")) {
+    ship.rotation += 3;
+  }
+}
+
+function createBG() {
+  //create background elements
+  for (let i=0; i<75; i++) {
+    let rocks = createSprite(random(-width, sceneW+width), random(-height, sceneH+height));
+    rocks.addImage("rocks", rocksImg);
+    bg.add(rocks);
+  }
+}
+
+function controlCamera() {
+  //camera follows ship
+  camera.position.x = ship.position.x;
+  camera.poistion.y = ship.position.y;
 }
