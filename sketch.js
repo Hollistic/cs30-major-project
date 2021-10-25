7aa9fdde8f582999c55233e853de26c3e9541290
Umair Khan <umairk2019@gmail.com>
2021-10-25 11:01:49 -0600

    fixed sprites, working on asteroids, added barrier

--- sketch.js
+++ sketch.js
@@ -14,6 +14,9 @@ let sceneH;
 let ship;
 let shipImage;
 
+//asteroids
+let asteroids;
+
 function setup() {
   //create window
   if (windowHeight < windowWidth) {
@@ -23,23 +26,29 @@ function setup() {
     createCanvas(windowWidth*0.9, windowWidth*0.9);
   }
 
-  sceneW = width + width/2;
-  sceneH = height + height/2;
+  sceneW = width + width*0.75;
+  sceneH = height + height*0.75;
 
 
   //load ship
-  shipRestImg = loadImage("assets/ship1.png");
-  shipThrustImg = loadImage("assets/ship1-thrust.png");
+  shipRestImg = loadImage("assets/playership.png");
+  shipThrustImg = loadImage("assets/playershipthrust.png");
   createShip(width/2, height/2, 50, 50, 5);
 
   //create asteroids group
+  asteroidImg = loadImage("assets/asteroid_big.png");
   asteroids = new Group();
 
   //create background group
   starsImg = loadImage("assets/star.png");
+  smallStarsImg = loadImage("assets/smallstar.png");
   bg = new Group();
 
   createBG();
+
+  for (let i=0; i<30; i++) {
+    createAsteroid(random(width), random(height));
+  }
 }
 
 function draw() {
@@ -47,7 +56,9 @@ function draw() {
   background(0);
 
   controlCamera();
+
   shipControls();
+  ship.bounce(asteroids);
 
   //ship is draw on top of background
   drawSprites(bg);
@@ -62,6 +73,20 @@ function createShip(x, y, w, h, speed) {
   ship.setCollider("rectangle", 0, 0, w, h);
   ship.maxSpeed = speed;
   ship.friction = 0.02;
+}
+
+function createAsteroid(x, y) {
+  let asteroid = createSprite(x, y);
+  asteroid.addImage("big-asteroids", asteroidImg);
+  asteroid.setCollider("circle", 0, 0, 2);
+  asteroid.mass = 2;
+  asteroid.setSpeed(2, random(360));
+  asteroid.rotationSpeed = 1;
+
+
+  //push to group
+  asteroids.add(asteroid);
+  return asteroid;
 }
 
 function shipControls() {
@@ -80,14 +105,20 @@ function shipControls() {
   if (keyDown("D")) {
     ship.rotation += 3;
   }
+
 }
 
 function createBG() {
   //create background elements
-  for (let i=0; i<200; i++) {
+  for (let i=0; i<100; i++) {
     let stars = createSprite(random(-width, sceneW+width), random(-height, sceneH+height));
     stars.addImage("stars", starsImg);
     bg.add(stars);
+  }
+  for (let i=0; i<400; i++) {
+    let smallStars = createSprite(random(-width, sceneW+width), random(-height, sceneH+height));
+    smallStars.addImage("small-stars", smallStarsImg);
+    bg.add(smallStars);
   }
 }
 
@@ -97,4 +128,17 @@ function controlCamera() {
   camera.zoom = 1.5;
   camera.position.x = ship.position.x;
   camera.position.y = ship.position.y;
+
+  if (ship.position.y < 0) {
+    ship.position.y = 0;
+  }
+  if (ship.position.y > height) {
+    ship.position.y = height;
+  }
+  if (ship.position.x < 0) {
+    ship.position.x = 0;
+  }
+  if (ship.position.x > width) {
+    ship.position.x = width;
+  }
 }
